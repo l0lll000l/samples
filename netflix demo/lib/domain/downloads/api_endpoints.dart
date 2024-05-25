a59b861917a55cf8f8c46d312bfe16fb0f7df768
l0lll000l <114205296+l0lll000l@users.noreply.github.com>
2024-05-25 10:56:06 +0530

    netflix

---
+++ netflix demo/lib/domain/downloads/api_endpoints.dart
@@ -0,0 +1,13 @@
+import 'package:netflix/core/constants.dart';
+import 'package:netflix/infrastructure/api_key.dart';
+
+class ApiEndPoint {
+  static const downloads = '$kbaseUrl/trending/all/day?api_key=$apiKey';
+  static const search = '$kbaseUrl/search/movie?api_key=$apiKey';
+  static const hotAndNewMovie = '$kbaseUrl/discover/movie?api_key=$apiKey';
+  static const hotAndNewTv = '$kbaseUrl/discover/tv?api_key=$apiKey';
+  static const trending = '$kbaseUrl/movie/now_playing?api_key=$apiKey';
+  static const popular = '$kbaseUrl/movie/top_rated?api_key=$apiKey';
+  static const upcoming = '$kbaseUrl/movie/upcoming?api_key=$apiKey';
+  static const tvShows = '$kbaseUrl/tv/airing_today?api_key=$apiKey';
+}
