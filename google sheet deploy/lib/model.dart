38d69e4d1a151907022adaea8c95e39ac2d9cb91
l0lll000l <114205296+l0lll000l@users.noreply.github.com>
2024-05-27 22:02:49 +0530

    api edit

--- google sheet deploy/lib/model.dart
+++ google sheet deploy/lib/model.dart
@@ -17,7 +17,8 @@ class MarioCharacter {
 
   static Future<List<MarioCharacter>> fetchAll() async {
     final response = await http.get(Uri.parse(
-        'https://script.googleusercontent.com/macros/echo?user_content_key=R2_O3gsEx0M3RmHRIyYzGQPXzwpxERHVWaQuJyIVtklZ1LhHQWCOf-iPwMw94i269WPrV7cqFsGdqPYOcKQpygkgQsO0veDpm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnG85V4XDV0yKLY7ku_30makKZLHLPgbEE6jU6qVJwdtzkM1c6Whli-Bo6DEX-dJ2hi9ZiS4vcjopNW0I7oiOSuxljd3AyhWQUw&lib=Mim0NEI_0MoZoAKMPD3v0q9ftfhwA5jNT'));
+        'url')); //-------------------------------------------paste api here
+
     if (response.statusCode == 200) {
       print(response);
       List<dynamic> jsonResponse = jsonDecode(response.body);
