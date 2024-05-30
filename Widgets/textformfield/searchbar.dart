95e6401d7769375bbe1609160698116d06e494f9
ATHUL M P <114205296+l0lll000l@users.noreply.github.com>
2024-05-30 19:39:37 +0530

    Update searchbar.dart

--- Widgets/textformfield/searchbar.dart
+++ Widgets/textformfield/searchbar.dart
@@ -1,1 +1,21 @@
 
+ SearchBar(
+                        textStyle: MaterialStatePropertyAll(
+                            TextStyle(color: Colors.white)),
+                        hintText: 'Search Location',
+                        hintStyle: const MaterialStatePropertyAll(
+                            TextStyle(color: Colors.white)),
+                        elevation: const MaterialStatePropertyAll(00.01),
+                        backgroundColor:
+                            const MaterialStatePropertyAll(Colors.transparent),
+                        trailing: {
+                          IconButton(
+                              onPressed: () {
+                                return print('search');
+                              },
+                              icon: const Icon(
+                                Icons.search,
+                                color: Colors.white,
+                              ))
+                        },
+                      ),
