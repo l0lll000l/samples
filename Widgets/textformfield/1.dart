df1f3b6d05ed37b77495a71b92fcb19c87733f7b
ATHUL M P <114205296+l0lll000l@users.noreply.github.com>
2024-05-30 11:36:51 +0530

    Update 1.dart

--- Widgets/textformfield/1.dart
+++ Widgets/textformfield/1.dart
@@ -12,3 +12,27 @@ TextFormField(
           return null;
         },
       ),
+/////////////////////////////////////////////////////////////////////
+Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Location',
+                    labelStyle: TextStyle(color: Colors.white),
+                    hintText: 'location',
+                    hintStyle: TextStyle(color: Colors.white),
+                    prefixIcon: Icon(
+                      Icons.cloud,
+                      color: Colors.white,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Search Location';
+                    }
+                    return null;
+                  },
+                ),
+              ),
