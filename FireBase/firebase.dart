cd0720b921226bd474d1dcadc77595822c7e70e6
ATHUL M P <114205296+l0lll000l@users.noreply.github.com>
2024-06-10 17:16:44 +0530

    Update firebase.dart

--- FireBase/firebase.dart
+++ FireBase/firebase.dart
@@ -1,1 +1,31 @@
 
+
+class Auth {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  User? get currentUser => _firebaseAuth.currentUser;
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+//-----------------------------------------------------------------------------Sign in
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.signInWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+//-----------------------------------------------------------------------------create User
+  Future<void> createUserWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await _firebaseAuth.createUserWithEmailAndPassword(
+        email: email, password: password);
+  }
+
+//-----------------------------------------------------------------------------log Out
+  Future<void> signOut() async {
+    await _firebaseAuth.signOut();
+  }
+}
+
