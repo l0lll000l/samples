79ab1816743307f31d773913f035617c9ee49e65
ATHUL M P <114205296+l0lll000l@users.noreply.github.com>
2024-06-10 17:41:52 +0530

    Update firebase.dart

--- FireBase/firebase.dart
+++ FireBase/firebase.dart
@@ -1,4 +1,74 @@
 
+final CollectionReference donor =
+      FirebaseFirestore.instance.collection('Users');
+//-------------------------------------------------------------------- Add Donor
+ void addDonor() {
+    final data = {
+      'name': 'Athul',
+      'phone': 9744690056,
+      'blood': 'b-',
+    };
+    donor.add(data);
+  }
+//-------------------------------------------------------------------- Update Donor
+void UpdateDonors(docid) {
+  final data = {
+    'name': donorName.text,
+    'phone': donorPhone.text,
+    'blood': selectedGroop,
+  };
+  donor.doc(docid).update(data);
+}
+//-------------------------------------------------------------------- Delete Donor
+void deleteDonor(docId) {
+  donor.doc(docId).delete();
+}
+//-------------------------------------------------------------------- read Donor
+ final DocumentSnapshot donorSnap = snapshot.data.docs[index];
+Text(donorSnap['phone'].toString() != null
+                        ? donorSnap['phone'].toString()
+                        : 'no phone number'),
+
+//-------------------------------------------------------------------- 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Auth {
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
