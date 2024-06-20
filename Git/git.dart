bc6adba707c3759e6039d5b8aa9bc4806281ef64
ATHUL M P <114205296+l0lll000l@users.noreply.github.com>
2024-06-20 15:38:40 +0530

    Create git.dart

---
+++ Git/git.dart
@@ -0,0 +1,21 @@
+git init                                       //initialize git
+git add main.dart                              //add file
+git commit                                     //add message , ESC :wq        to save
+git status                                     //Changes to be committed:
+                                               // (use "git rm --cached <file>..." to unstage)
+                                               //new file:   main.dart
+
+git commit                                     //[master (root-commit) bc83faf] create main.dart
+                                               // 1 file changed, 0 insertions(+), 0 deletions(-)
+                                               //create mode 100644 main.dart
+//-----------------------------------------------------------------------------------------------------------------
+  git add main.dart                            //add file
+  git status                                   //status check
+  git commit -m 'print'                        //message add
+  git log                                      // print previous logs
+//---------------------------------------------------------------------------------------------------
+  git add .                                    //add all files
+  git checkout a1205c264d96490b0c429015aa0bc41d1e86c94f    //detatch head
+  git checkout master                          //back to normal
+  git diff                                     //recently modified things will show
+  git reset HEAD~2 --hard                      //remove 2 commits      
