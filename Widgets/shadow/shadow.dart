c5fb6e0f3c117772329cc9f9d277bc8d1adbd926
ATHUL M P <114205296+l0lll000l@users.noreply.github.com>
2024-06-25 15:15:46 +0530

    Create shadow.dart

---
+++ Widgets/shadow/shadow.dart
@@ -0,0 +1,23 @@
+  Container(
+      width: 180,
+      height: 200,
+      decoration: BoxDecoration(
+          boxShadow: [TShadowStyle.verticalProductShadow],         //vertical prodct shadow
+          color: dark ? TColors.darkerGrey : TColors.light,
+          borderRadius: BorderRadius.circular(TSizes.productImageRadius)),
+    );
+-------------------------------------------------------------
+
+class TShadowStyle {
+  static final verticalProductShadow = BoxShadow(                    
+      color: TColors.darkerGrey.withOpacity(0.1),
+      blurRadius: 50,
+      spreadRadius: 7,
+      offset: const Offset(0, 2));
+  static final horrizondalProductShadow = BoxShadow(
+      color: TColors.darkerGrey.withOpacity(0.1),
+      blurRadius: 50,
+      spreadRadius: 7,
+      offset: const Offset(0, 2));
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
