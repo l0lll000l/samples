682e03182b5505740af144a52dd40ab77c01b50e
ATHUL M P <114205296+l0lll000l@users.noreply.github.com>
2024-06-28 20:02:44 +0530

    Update dependency.dart

--- Widgets/dependency.dart
+++ Widgets/dependency.dart
@@ -1,2 +1,3 @@
-readmore      =   https://pub.dev/packages/readmore/install            = wrap text
-carousel_slider      =  https://pub.dev/packages/carousel_slider       =  scroll images or widgets
+readmore                   =   https://pub.dev/packages/readmore/install             = wrap text
+carousel_slider            =  https://pub.dev/packages/carousel_slider               =  scroll images or widgets   -used showing ad
+smooth_page_indicator      = https://pub.dev/packages/smooth_page_indicator          =  page change indication - usefull
