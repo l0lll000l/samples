bcb42fd7a749cf84d7857395aba61d5f3647842f
ATHUL M P <114205296+l0lll000l@users.noreply.github.com>
2024-06-28 22:10:30 +0530

    Update dependency.dart

--- Widgets/dependency.dart
+++ Widgets/dependency.dart
@@ -1,3 +1,4 @@
 readmore                   =   https://pub.dev/packages/readmore/install             = wrap text
 carousel_slider            =  https://pub.dev/packages/carousel_slider               =  scroll images or widgets   -used showing ad
 smooth_page_indicator      = https://pub.dev/packages/smooth_page_indicator          =  page change indication - usefull
+flutter_rating_bar         = https://pub.dev/packages/flutter_rating_bar/install     =  rating bar
