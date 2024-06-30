0529dae1922112217f317a76f6588251009cd198
ATHUL M P <114205296+l0lll000l@users.noreply.github.com>
2024-06-30 20:55:39 +0530

    Update dependency.dart

--- Widgets/dependency.dart
+++ Widgets/dependency.dart
@@ -2,3 +2,4 @@ readmore                   =   https://pub.dev/packages/readmore/install        
 carousel_slider            =  https://pub.dev/packages/carousel_slider               =  scroll images or widgets   -used showing ad
 smooth_page_indicator      = https://pub.dev/packages/smooth_page_indicator          =  page change indication - usefull
 flutter_rating_bar         = https://pub.dev/packages/flutter_rating_bar/install     =  rating bar
+lottie                     = https://pub.dev/packages/lottie                         =  animations
